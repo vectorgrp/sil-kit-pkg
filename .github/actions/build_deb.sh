2396b47d0165b9add32ce2db3e90000f83b59f4f
Jan Kraemer <jan.kraemer@vector.com>
2024-01-23 15:11:17 +0100

    build_deb.sh: rewrite for local packaging
    * Support local sil-kit orig tarballs
    * support local sil-kit-pkg directories
    
    Signed-off-by: Jan Kraemer <jan.kraemer@vector.com>

--- .github/actions/build_deb.sh
+++ .github/actions/build_deb.sh
@@ -2,32 +2,89 @@
 
 echo "Building the SIL Kit packages"
 
-orig_tarball=${SILKIT_SOURCE_URL}/${SILKIT_VERSION}-${SILKIT_VERSION_SUFFIX}/libsilkit_${SILKIT_VERSION}.orig.tar.xz
-wget --no-proxy --no-check-certificate $orig_tarball
-ret_val=$?
-if [ "$ret_val" != '0' ] ; then
-    echo "SILKIT-PKG: Could not get orig tarball from $orig_tarball"
-    exit 64
+#Check whether SILKIT SOURCE is local
+orig_found=false
+if [ -f ${SILKIT_SOURCE_URL}/libsilkit_${SILKIT_VERSION}.orig.tar.xz ] ; then
+    echo "Found orig tarball at ${SILKIT_SOURCE_URL}/libsilkit_${SILKIT_VERSION}.orig.tar.xz"
+    orig_tarball=${SILKIT_SOURCE_URL}/libsilkit_${SILKIT_VERSION}.orig.tar.xz
+    cp $orig_tarball ./
+    mkdir -p libsilkit-${SILKIT_VERSION}
+    tar -xvf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION}
+    ret_val=$?
+    if [ "$ret_val" != '0' ] ; then
+        echo "SILKIT-PKG: Could not untar orig tarball $orig_tarball"
+        exit 64
+    fi
+    orig_found=true
 fi
 
-mkdir -p libsilkit-${SILKIT_VERSION}
-tar -xvf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION}
-ret_val=$?
-if [ "$ret_val" != '0' ] ; then
-    echo "SILKIT-PKG: Could not untar orig tarball $orig_tarball"
-    exit 64
+# Check git before https/ftp
+if ! $orig_found ; then
+
+    if git ls-remote -hq ${SILKIT_SOURCE_URL} ; then
+        #git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "sil-kit/v${SILKIT_VERSION}" libsilkit-${SILKIT_VERSION}
+        git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "main" libsilkit-${SILKIT_VERSION}
+        ret_val=$?
+        if [ "$ret_val" != '0' ] ; then
+            echo "SILKIT-PKG: Could get SIL Kit sources at ${SILKIT_SOURCE_URL}:silkit/v${SILKIT_VERSION}"
+            exit 64
+        fi
+        tar -cJf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION} .
+        ret_val=$?
+        if [ "$ret_val" != '0' ] ; then
+            echo "SILKIT-PKG: Could create orig tarball $orig_tarball"
+            exit 64
+        fi
+        orig_found=true
+    fi
 fi
 
-git -c http.sslVerify=false clone ${SILKIT_PACKAGING_REPO} -b ${SILKIT_PACKAGING_REPO_BRANCH}
-ret_val=$?
-if [ "$ret_val" != '0' ] ; then
-    echo "SILKIT-PKG: Could not clone $SILKIT_PACKAGING_REPO : $SILKIT_PACKAGING_REPO_BRANCH"
-    exit 64
+# Check wgettable orig
+if ! $orig_found ; then
+
+    if wget -q --method=HEAD ${SILKIT_SOURCE_URL} ; then
+        orig_tarball=$SILKIT_SOURCE_URL
+        wget --no-proxy --no-check-certificate $orig_tarball
+        orig_found=true
+
+        mkdir -p libsilkit-${SILKIT_VERSION}
+        tar -xvf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION}
+        ret_val=$?
+        if [ "$ret_val" != '0' ] ; then
+            echo "SILKIT-PKG: Could not untar orig tarball $orig_tarball"
+            exit 64
+        fi
+    fi
+    echo "SILKIT-PKG: USED HTTPS BITCHES!"
+fi
+
+if ! $orig_found ; then
+    echo "SILKIT-PKG: Could not get the orig tarball. Exiting!"
+    exit 128
+fi
+
+
+debian_path="${SILKIT_PKG_URL}"
+silkit_pkg_found=false
+if [ -d ${SILKIT_PKG_URL} ] ; then
+    silkit_pkg_found=true
+fi
+
+if ! $silkit_pkg_found ; then
+    if git ls-remote -hq ${SILKIT_PKG_URL} ; then
+        git -c http.sslVerify=false clone --depth=1 ${SILKIT_PKG_URL} -b ${SILKIT_PKG_BRANCH} sil-kit-pkg
+        ret_val=$?
+        if [ "$ret_val" != '0' ] ; then
+            echo "SILKIT-PKG: Could not clone $SILKIT_PKG_REPO : $SILKIT_PKG_BRANCH"
+            exit 64
+        fi
+        debian_path="./sil-kit-pkg"
+    fi
 fi
 
 SILKIT_PACKAGE_REPO_NAME=$(echo ${SILKIT_PACKAGING_REPO} | awk -F/ '{print $NF}' | awk -F. '{print $1}')
 echo "PACKAGING REPO NAME ${SILKIT_PACKAGE_REPO_NAME}"
-mv ./${SILKIT_PACKAGE_REPO_NAME}/debian libsilkit-${SILKIT_VERSION}
+cp -r ${debian_path}/debian libsilkit-${SILKIT_VERSION}
 
 cd libsilkit-${SILKIT_VERSION}/
 
