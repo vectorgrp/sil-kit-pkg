c3b66ad26ec1284e62c017da15e52148321345bc
Jan Kraemer <jan.kraemer@vector.com>
2024-01-24 10:47:19 +0100

    build_deb: rewrite of the build logic
    * GIT as sole source of truth for sources
    * Better naming for the env variables
    
    Signed-off-by: Jan Kraemer <jan.kraemer@vector.com>

--- .github/actions/build_deb.sh
+++ .github/actions/build_deb.sh
@@ -2,90 +2,49 @@
 
 echo "Building the SIL Kit packages"
 
-#Check whether SILKIT SOURCE is local
-orig_found=false
-if [ -f ${SILKIT_SOURCE_URL}/libsilkit_${SILKIT_VERSION}.orig.tar.xz ] ; then
-    echo "Found orig tarball at ${SILKIT_SOURCE_URL}/libsilkit_${SILKIT_VERSION}.orig.tar.xz"
-    orig_tarball=${SILKIT_SOURCE_URL}/libsilkit_${SILKIT_VERSION}.orig.tar.xz
-    cp $orig_tarball ./
-    mkdir -p libsilkit-${SILKIT_VERSION}
-    tar -xvf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION}
+if git ls-remote -hq ${SILKIT_SOURCE_URL} ; then
+    #git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "sil-kit/v${SILKIT_VERSION}" libsilkit-${SILKIT_VERSION}
+    git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "main" libsilkit-${SILKIT_VERSION}
     ret_val=$?
     if [ "$ret_val" != '0' ] ; then
-        echo "SILKIT-PKG: Could not untar orig tarball $orig_tarball"
+        echo "SILKIT-PKG: Could get SIL Kit sources at ${SILKIT_SOURCE_URL}:silkit/v${SILKIT_VERSION}"
+        exit 64
+    fi
+    tar -cJf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION} .
+    ret_val=$?
+    if [ "$ret_val" != '0' ] ; then
+        echo "SILKIT-PKG: Could create orig tarball $orig_tarball"
         exit 64
     fi
     orig_found=true
 fi
 
-# Check git before https/ftp
 if ! $orig_found ; then
-
-    if git ls-remote -hq ${SILKIT_SOURCE_URL} ; then
-        #git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "sil-kit/v${SILKIT_VERSION}" libsilkit-${SILKIT_VERSION}
-        git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "main" libsilkit-${SILKIT_VERSION}
-        ret_val=$?
-        if [ "$ret_val" != '0' ] ; then
-            echo "SILKIT-PKG: Could get SIL Kit sources at ${SILKIT_SOURCE_URL}:silkit/v${SILKIT_VERSION}"
-            exit 64
-        fi
-        tar -cJf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION} .
-        ret_val=$?
-        if [ "$ret_val" != '0' ] ; then
-            echo "SILKIT-PKG: Could create orig tarball $orig_tarball"
-            exit 64
-        fi
-        orig_found=true
-    fi
-fi
-
-# Check wgettable orig
-if ! $orig_found ; then
-
-    if wget -q --method=HEAD ${SILKIT_SOURCE_URL} ; then
-        orig_tarball=$SILKIT_SOURCE_URL
-        wget --no-proxy --no-check-certificate $orig_tarball
-        orig_found=true
-
-        mkdir -p libsilkit-${SILKIT_VERSION}
-        tar -xvf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION}
-        ret_val=$?
-        if [ "$ret_val" != '0' ] ; then
-            echo "SILKIT-PKG: Could not untar orig tarball $orig_tarball"
-            exit 64
-        fi
-    fi
-    echo "SILKIT-PKG: USED HTTPS BITCHES!"
-fi
-
-if ! $orig_found ; then
-    echo "SILKIT-PKG: Could not get the orig tarball. Exiting!"
+    echo "SILKIT-PKG: Could not get the SIL Kit sources! Exiting!"
     exit 128
 fi
 
 
-debian_path="${SILKIT_PKG_URL}"
+debian_path="${SILKIT_PKG_URL}/debian"
 silkit_pkg_found=false
-if [ -d ${SILKIT_PKG_URL} ] ; then
+# Check if the provided path has a debian directory
+if [ -d ${debian_path} ] ; then
     silkit_pkg_found=true
 fi
 
 if ! $silkit_pkg_found ; then
     if git ls-remote -hq ${SILKIT_PKG_URL} ; then
-        git -c http.sslVerify=false clone --depth=1 ${SILKIT_PKG_URL} -b ${SILKIT_PKG_BRANCH} sil-kit-pkg
+        git -c http.sslVerify=false clone --depth=1 ${SILKIT_PKG_URL} -b ${SILKIT_PKG_REF} sil-kit-pkg
         ret_val=$?
         if [ "$ret_val" != '0' ] ; then
-            echo "SILKIT-PKG: Could not clone $SILKIT_PKG_REPO : $SILKIT_PKG_BRANCH"
+            echo "SILKIT-PKG: Could not clone $SILKIT_PKG_URL : $SILKIT_PKG_REF"
             exit 64
         fi
-        debian_path="./sil-kit-pkg"
+        debian_path="./sil-kit-pkg/debian"
     fi
 fi
 
-SILKIT_PACKAGE_REPO_NAME=$(echo ${SILKIT_PACKAGING_REPO} | awk -F/ '{print $NF}' | awk -F. '{print $1}')
-echo "PACKAGING REPO NAME ${SILKIT_PACKAGE_REPO_NAME}"
-cp -r ${debian_path}/debian libsilkit-${SILKIT_VERSION}
-
+cp -r ${debian_path} libsilkit-${SILKIT_VERSION}
 cd libsilkit-${SILKIT_VERSION}/
 
 echo "Running dh_make"
@@ -103,6 +62,3 @@ if [ "$ret_val" != '0' ] ; then
     echo "SILKIT-PKG: \"debuild -us -uc\" exit code $ret_val"
     exit 64
 fi
-
-#echo "Copying artifacts"
-#SILKIT_VERSION_MAJOR=$(echo ${SILKIT_VERSION} | cut -d '.' -f1)
