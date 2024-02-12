57e9d9b5847165745ee37b618a6c723f3a3eb31c
Jan Kraemer <jan.kraemer@vector.com>
2024-02-12 14:49:29 +0100

    build_deb: remove unnecessary checks
    Some checks were done twice to accomodate logic in the script which has
    since been deleted. Such as trying multiple methods to get the source
    tarball or the packaging repo. This checks have now been retired
    
    Signed-off-by: Jan Kraemer <jan.kraemer@vector.com>

--- .github/actions/build_deb.sh
+++ .github/actions/build_deb.sh
@@ -9,21 +9,15 @@ debian_path="${SILKIT_PKG_URL}/debian"
 
 silkit_pkg_found=false
 # Check if the provided path has a debian directory
-if [ -d ${debian_path} ] ; then
-    silkit_pkg_found=true
-fi
-
-if ! $silkit_pkg_found ; then
-    echo "SILKIT-PKG: Trying git sil-kit-pkg"
-    if git ls-remote -hq ${SILKIT_PKG_URL} ; then
-        git -c http.sslVerify=false clone --depth=1 ${SILKIT_PKG_URL} -b ${SILKIT_PKG_REF} sil-kit-pkg
-        ret_val=$?
-        if [ "$ret_val" != '0' ] ; then
-            echo "SILKIT-PKG: Could not clone $SILKIT_PKG_URL : $SILKIT_PKG_REF"
-            exit 64
-        fi
-        debian_path="./sil-kit-pkg/debian"
+if [ ! -d ${debian_path} ] ; then
+    echo "SILKIT-PKG: Could not find local debian directory, trying to clone the sil-kit-pkg git repo!"
+    git -c http.sslVerify=false clone --depth=1 ${SILKIT_PKG_URL} -b ${SILKIT_PKG_REF} sil-kit-pkg
+    ret_val=$?
+    if [ "$ret_val" != '0' ] ; then
+        echo "SILKIT-PKG: Could not clone $SILKIT_PKG_URL : $SILKIT_PKG_REF"
+        exit 64
     fi
+    debian_path="./sil-kit-pkg/debian"
 fi
 
 
@@ -41,25 +35,17 @@ if [ -n "${CI_RUN+1}" ] ; then
     echo "silkit_debian_revision=${SILKIT_DEBIAN_REVISION}" >> "$GITHUB_OUTPUT"
 fi
 
-if git ls-remote -hq ${SILKIT_SOURCE_URL} ; then
-    git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "sil-kit/v${SILKIT_VERSION}" libsilkit-${SILKIT_VERSION}
-    ret_val=$?
-    if [ "$ret_val" != '0' ] ; then
-        echo "SILKIT-PKG: Could get SIL Kit sources at ${SILKIT_SOURCE_URL}:silkit/v${SILKIT_VERSION}"
-        exit 64
-    fi
-    tar -cJf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION} .
-    ret_val=$?
-    if [ "$ret_val" != '0' ] ; then
-        echo "SILKIT-PKG: Could create orig tarball $orig_tarball"
-        exit 64
-    fi
-    orig_found=true
+git -c http.sslVerify=false clone ${SILKIT_SOURCE_URL} --depth=1 -b "sil-kit/v${SILKIT_VERSION}" libsilkit-${SILKIT_VERSION}
+ret_val=$?
+if [ "$ret_val" != '0' ] ; then
+    echo "SILKIT-PKG: Could get SIL Kit sources at ${SILKIT_SOURCE_URL}:silkit/v${SILKIT_VERSION}"
+    exit 64
 fi
-
-if ! $orig_found ; then
-    echo "SILKIT-PKG: Could not get the SIL Kit sources! Exiting!"
-    exit 128
+tar -cJf libsilkit_${SILKIT_VERSION}.orig.tar.xz -C libsilkit-${SILKIT_VERSION} .
+ret_val=$?
+if [ "$ret_val" != '0' ] ; then
+    echo "SILKIT-PKG: Could create orig tarball $orig_tarball"
+    exit 64
 fi
 
 cp -r ${debian_path} libsilkit-${SILKIT_VERSION}
