394dffff894b79fd420e0f0ed7e5d91bb5715743
Jan Kraemer <jan.kraemer@vector.com>
2024-11-05 08:58:38 +0100

    ci: add automatic rpm smoke tests
    Issues: SILKIT-1597
    
    Signed-off-by: Jan Kraemer <jan.kraemer@vector.com>

--- tests/test_deb.py
+++ tests/test_deb.py
@@ -21,8 +21,9 @@ logger = logging.getLogger(__name__)
 def create_arg_parser() -> ArgumentParser:
     ap = ArgumentParser("PackageTest")
 
-    ap.add_argument("--deb-directory", type=Path, required=True)
     ap.add_argument("--package-directory", type=Path, required=True)
+    ap.add_argument("--test-directory", type=Path, required=True)
+    ap.add_argument("--distro", type=str, required=True)
     return ap
 
 def install_debs(deb_directory):
@@ -37,6 +38,19 @@ def install_debs(deb_directory):
         subprocess.run(args, check=True)
     except subprocess.CalledProcessError:
         print("Could not install the DEB files. Exiting with ERROR!")
+        exit(64)
+
+def install_rpms(rpm_directory):
+    print("\nInstall SilKit Fedora/Alma Linux packages")
+    print("--------------------------------\n", flush=True)
+    rpms = rpm_directory.glob('*.x86_64.rpm')
+    str_args = [str(rpm.resolve()) for rpm in rpms]
+    args = ['dnf', 'install', '-y'] + str_args
+
+    try:
+        subprocess.run(args, check=True)
+    except subprocess.CalledProcessError:
+        print("Could not install the RPM files. Exiting with ERROR!")
         exit(64)
 
 def run_registry():
@@ -67,7 +81,10 @@ def close_registry(reg_proc: subprocess.Popen):
     else:
         print("All is fine, exiting gracefully!")
 
-def build_test(test_dir):
+def build_test(test_dir, distro: str):
+
+    cc = "clang-10" if distro.lower() == "ubuntu" else "clang"
+    cxx = "clang++-10" if distro.lower() == "ubuntu" else "clang++"
 
     print("\nBuild the SilKit Test program")
     print("--------------------------------\n", flush=True)
@@ -78,8 +95,8 @@ def build_test(test_dir):
 
     # Configure the build
     current_env = os.environ.copy()
-    current_env["CC"]="clang-10"
-    current_env["CXX"]="clang++-10"
+    current_env["CC"]=cc
+    current_env["CXX"]=cxx
     build_dir = test_dir / '_build'
     subprocess.run(['cmake', '-GNinja', '../'], env=current_env, cwd=build_dir, check=True)
 
@@ -94,23 +111,36 @@ def run_test(build_dir):
     try:
         subprocess.run(['./Test'], cwd=build_dir, check=True)
     except:
-        print("Test did not run to completion. Your problem now!")
-        exit(64)
+        raise RuntimeError("Test did not run to completion. Your problem now!")
 
 def main():
     argument_parser = create_arg_parser()
     args =argument_parser.parse_args()
 
     # Check deb install
-    install_debs(args.deb_directory)
-    registry = run_registry()
+    if args.distro.lower() == "ubuntu":
+        install_debs(args.package_directory)
+    elif args.distro.lower() == "fedora":
+        install_rpms(args.package_directory)
+    else:
+        print(f"{args.distro} is currently not supported. Exiting!")
+        exit(64)
+
 
     # Check demo program (compilation/link/execution)
-    test_dir = args.package_directory / 'tests'
+    test_dir = args.test_directory
     build_dir = test_dir / '_build'
-    build_test(test_dir)
-    run_test(build_dir)
+    build_test(test_dir, args.distro)
+    registry = run_registry()
+    ret_code = 0
+    try:
+        run_test(build_dir)
+    except RuntimeError as rte:
+        print(f"ERROR: {str(rte)}")
+        ret_code = 64 
+
     close_registry(registry)
+    exit(ret_code)
 
 if __name__ == "__main__":
     main()
